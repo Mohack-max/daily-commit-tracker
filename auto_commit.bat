c2bc6be7546bbe443cba06636235c044c4fa75b5
Mohack-max <mohgithub@gmail.com>
2025-04-05 01:46:07 +0530

    Auto commit 1: Sat 04/05/2025

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,6 +1,16 @@
 @echo off
 cd /d %~dp0
-echo Commit made on %date% at %time% >> log.txt
-git add .
-git commit -m "Auto commit: %date%"
+
+setlocal enabledelayedexpansion
+
+:: Number of fake commits you want to make
+set COUNT=10
+
+for /L %%i in (1,1,%COUNT%) do (
+    echo Commit %%i made on %date% at %time% >> log.txt
+    git add .
+    git commit -m "Auto commit %%i: %date%"
+    timeout /t 1 >nul
+)
+
 git push origin main
